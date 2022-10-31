d010055aa42d2e52c79b6bb02ef220c3d93949a7
Ashwini Kumar <priyanshuashwinikumar@gmail.com>
2022-10-31 19:42:06 +0530

    feat: new categories for shop added

--- apps/admin/src/app/pages/orders/orders-detail/orders-detail.component.ts
+++ apps/admin/src/app/pages/orders/orders-detail/orders-detail.component.ts
@@ -77,4 +77,72 @@ export class OrdersDetailComponent implements OnInit, OnDestroy {
         }
       );
   }
+  
+  @Component({
+  selector: 'admin-orders-list',
+  templateUrl: './orders-list.component.html',
+  styles: []
+})
+export class OrdersListComponent implements OnInit, OnDestroy {
+  orders: Order[] = [];
+  orderStatus = ORDER_STATUS;
+  endsubs$: Subject<any> = new Subject();
+
+  constructor(
+    private ordersService: OrdersService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this._getOrders();
+  }
+  ngOnDestroy() {
+    this.endsubs$.next();
+    this.endsubs$.complete();
+  }
+
+  _getOrders() {
+    this.ordersService
+      .getOrders()
+      .pipe(takeUntil(this.endsubs$))
+      .subscribe((orders) => {
+        this.orders = orders;
+      });
+  }
+
+  showOrder(orderId) {
+    this.router.navigateByUrl(`orders/${orderId}`);
+  }
+
+  deleteOrder(orderId: string) {
+    this.confirmationService.confirm({
+      message: 'Do you want to Delete this Order?',
+      header: 'Delete Order',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.ordersService
+          .deleteOrder(orderId)
+          .pipe(takeUntil(this.endsubs$))
+          .subscribe(
+            () => {
+              this._getOrders();
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Success',
+                detail: 'Order is deleted!'
+              });
+            },
+            () => {
+              this.messageService.add({
+                severity: 'error',
+                summary: 'Error',
+                detail: 'Order is not deleted!'
+              });
+            }
+          );
+      }
+    });
+  }
 }
